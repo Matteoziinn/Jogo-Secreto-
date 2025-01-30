ba6eb34cf7b4c07b5295f87b5fc4346a27b2281e
Matteo <146030856+Matteoziinn@users.noreply.github.com>
2025-01-30 16:50:54 -0300

    app.js

--- app.js
+++ app.js
@@ -1,8 +1,32 @@
+/**
+ * Lista de números sorteados anteriormente.
+ * @type {number[]}
+ */
 let listaNumerosSorteados = [];
+
+/**
+ * Limite máximo para o número secreto.
+ * @type {number}
+ */
 let numeroLimites = 100;
+
+/**
+ * Número secreto gerado aleatoriamente.
+ * @type {number}
+ */
 let numeroSecreto = gerarNumeroAleatorio();
+
+/**
+ * Contador de tentativas do usuário.
+ * @type {number}
+ */
 let tentativas = 1;
 
+/**
+ * Exibe um texto em um elemento HTML e utiliza a síntese de fala para narrar o texto.
+ * @param {string} tag - Seletor do elemento HTML onde o texto será exibido.
+ * @param {string} texto - Texto a ser exibido e narrado.
+ */
 function exibirTexto(tag, texto){
     let campo = document.querySelector(tag);
     campo.innerHTML = texto;
@@ -16,16 +40,20 @@ function exibirTexto(tag, texto){
     }
 }
 
+/**
+ * Exibe a mensagem inicial do jogo.
+ */
 function exibirMensagem(){
     exibirTexto('h1', 'Jogo Do Número Secreto');
     exibirTexto('p', 'Tente adivinhar o número secreto entre 1 e 100.');
 }
-
 exibirMensagem();
 
+/**
+ * Verifica o chute do usuário e exibe a mensagem correspondente.
+ */
 function verificarChute(){
     let chute = document.querySelector('input').value;
-    
     if (chute == numeroSecreto) {
         exibirTexto('h1', 'Parabéns! Você acertou!');
         let palavraTentativa = tentativas > 1 ? 'tentativas' : 'tentativa';
@@ -43,10 +71,13 @@ function verificarChute(){
     }
 }
 
+/**
+ * Gera um número aleatório entre 1 e o limite definido e verifica se já foi sorteado anteriormente.
+ * @returns {number} - Número aleatório gerado.
+ */
 function gerarNumeroAleatorio(){
     let numeroEscolhido = parseInt(Math.random() * numeroLimites + 1);
     let quantidadeDeElementosaNaLista = listaNumerosSorteados.length;
-
     if(quantidadeDeElementosaNaLista == numeroLimites){
         listaNumerosSorteados = [];
     }
@@ -59,10 +90,16 @@ function gerarNumeroAleatorio(){
     }
 }
 
+/**
+ * Limpa o campo de input.
+ */
 function limparCampo(){
     document.querySelector('input').value = '';
 }
 
+/**
+ * Reinicia o jogo, gerando um novo número secreto e resetando as tentativas.
+ */
 function reiniciarJogo(){
     numeroSecreto = gerarNumeroAleatorio();
     limparCampo();
